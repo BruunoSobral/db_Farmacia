cc93aacf016cb237d13ec3e859b8867abd9727d0
BruunoSobral <brunocole19@gmail.com>
2022-03-31 17:02:37 -0300

    Atualizado

--- db_farmacia/src/main/java/org/generation/db_farmacia/model/Produto.java
+++ db_farmacia/src/main/java/org/generation/db_farmacia/model/Produto.java
@@ -1,7 +1,6 @@
 package org.generation.db_farmacia.model;
 
 import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +9,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
